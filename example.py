ea551ecc4ba65730c30a5fa7422fbbf3448ed76d
lucasemdb <lucas@ebi.ac.uk>
2024-08-23 16:39:50 +0100

    Update the example to use a light mmCIF example file

--- example.py
+++ example.py
@@ -8,14 +8,16 @@ def cross_checker(category_name_1, category_name_2):
     print(f"\nCross-checking categories: {category_name_1} and {category_name_2}")
     
 def modify_data(modified_file):
-    # Assuming there is a data block named '7XJP'
-    data_block = modified_file['7XJP']  # Replace '7XJP' with the actual block name
+    # Assuming there is a data block named ''
+    data_block = modified_file.data_
 
     # Assuming there is a category named '_database_2'
-    category = data_block['_database_2']  # Replace '_database_2' with the actual category name
+    category = data_block._database_2
 
     # Modify an item in the category
-    category._items['database_id'][-1] = 'NEWDB'
+    database_ids = list(category.database_id)
+    database_ids[-1] = 'NEWDB'
+    category.database_id = iter(database_ids)
 
 # Initialize the ValidatorFactory and register validators
 validator_factory = ValidatorFactory()
@@ -24,7 +26,7 @@ validator_factory = ValidatorFactory()
 handler = MMCIFHandler(atoms=True, validator_factory=validator_factory)
 
 # Parse the file with specific categories
-file = handler.parse('/Users/lucas/Desktop/em/emd_33233.cif')#, categories=['_database_2', '_atom_site'])
+file = handler.parse('/Users/lucas/Desktop/em/example.cif')#, categories=['_database_2', '_atom_site'])
 
 # Print data blocks
 print(f"\nData blocks: {file.blocks}")
@@ -32,14 +34,14 @@ print(f"\nData blocks: {file.blocks}")
 # Print file content
 print(f"\nFile content: {file.data}")
 
-# Accessing the DataBlock named '7XJP'
-print(f"\nData Block: {file.data_7XJP}")
+# Accessing the DataBlock named ''
+print(f"\nData Block: {file.data_}")
 
 # Accessing the first DataBlock in the file
 print(f"\nFirst Data Block: {file.data[0]}")
 
-category = file.data_7XJP._database_2
-other_category = file.data_7XJP._atom_site
+category = file.data_._database_2
+other_category = file.data_._atom_site
 
 # Register the validators and cross-checkers
 validator_factory.register_validator(category.name, category_validator)
@@ -63,12 +65,12 @@ for data_block in file:
 modify_data(file)
 
 # Write the updated content back to a new mmCIF file
-with open('/Users/lucas/Desktop/em/modified_emd_33233.cif', 'w') as f:
+with open('/Users/lucas/Desktop/em/modified_example.cif', 'w') as f:
     handler.file_obj = f
     handler.write(file)
 
 # Verify the changes
-modified_file = handler.parse('/Users/lucas/Desktop/em/modified_emd_33233.cif')#, categories=['_database_2', '_atom_site'])
+modified_file = handler.parse('/Users/lucas/Desktop/em/modified_example.cif')#, categories=['_database_2', '_atom_site'])
 for data_block in modified_file:
     for category in data_block:
         print(f"\nCategory: {category}")
